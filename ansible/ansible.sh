b71db2c1d610e6e700782a7a0c9169612538a019
Ken <kentyiro@outlook.com>
2023-12-05 15:27:57 -0500

    modified asible configuration and deployment

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -1,3 +1,13 @@
-kubectl apply -f ansible-configmap.yaml
-kubectl apply -f ansible-deployment.yaml
-kubectl apply -f ansible-service.yaml
+#!/bin/bash
+
+# Check if the Ansible deployment is running, if so, delete it, otherwise create it
+if kubectl get deployments | grep ansible-deployment; then
+    kubectl delete -f ansible-configmap.yaml
+    kubectl delete -f ansible_deployment.yaml
+    kubectl delete -f ansible-service.yaml
+else
+    kubectl apply -f ansible-configmap.yaml
+    kubectl apply -f ansible-deployment.yaml
+    kubectl apply -f ansible-service.yaml
+fi
+

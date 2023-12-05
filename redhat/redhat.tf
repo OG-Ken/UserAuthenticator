8623132d5a8d43cf874294d18fe5cad39fd01798
Ken <kentyiro@outlook.com>
2023-12-05 09:45:16 -0500

    modified ec2 instance type and requirements

--- redhat/redhat.tf
+++ redhat/redhat.tf
@@ -4,10 +4,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "rhel_instance" {
-  ami             = "ami-xxxxxxxxxxxxxxxx"  # Replace with the appropriate RHEL AMI
-  instance_type   = "t2.micro"
-  key_name        = "your-key-pair"
-  security_group  = ["your-security-group"]  # Add security groups
+  ami             = "ami-XXXXXX"  # Replace with the appropriate RHEL AMI
+  instance_type   = "t2.medium"   # Update to a larger instance type
+  key_name        = "key-pair"
+  #security_group  = ["security-group"]  # Add security groups
+  ebs_optimized   = true          # Enable EBS optimization
+  iam_instance_profile = "iam-role" # Assign an IAM role
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                # Add other configuration commands
+                EOF
 
   tags = {
     Name = "RedHatInstance"
